56c97d0940c6bc47ff216ac56bca30713a8fa7cf
karima arslane <kamime.93@hotmail.fr>
2022-02-01 21:09:51 +0100

    afficher les produits du panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -12,17 +12,20 @@ if(saveProduct === null) {
     //si le panier n'est pas vide afficher les produits dans le localstorage    
 
     for(let k = 0; k < saveProduct.length; k++) {
-        
+        fetch("http://localhost:3000/api/products/" + saveProduct[k].id)
+        .then(response => response.json()
+        .then(data =>{
+            console.log(data)
          panier.innerHTML += `
         <article class="cart__item" data-id="${saveProduct[k].id}" data-color="${saveProduct[k].colors}">
         <div class="cart__item__img">
-        <img src=${saveProduct[k].image} alt=${saveProduct[k].altTxt}>
+        <img src=${data.imageUrl} alt=${data.altTxt}>
         </div>
         <div class="cart__item__content">
         <div class="cart__item__content__description">
-        <h2>${saveProduct[k].title}</h2>
+        <h2>${data.title}</h2>
         <p>${saveProduct[k].colors}</p>
-        <p>${saveProduct[k].price}</p>
+        <p>${data.price}</p>
         </div>
         <div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity">
@@ -34,13 +37,40 @@ if(saveProduct === null) {
         </div>
         </div>
         </div>
-        </article> `;
-    }    
-    
+        </article>`;
+        
+    }
+   ))};
 }
 
+    //supprimer un article
+    function productDElete() {
+        let btnDelete = document.getElementsByClassName(".deleteItem");
+         console.log(btnDelete)
+         for (let n = 0; n < btnDelete.length; n++) {
+            btnDelete[n].addEventListener("click", (event) => {
+                event.preventDefault();
+    
+                //Selection de l'element à supprimer en fonction de son id ET sa couleur
+                let idDelete = saveProduct[n].id;
+                let colorsDelete = saveProduct[n].colors;
+    
+               saveProduct = saveProduct.filter(
+                    (el) => el.id !== idDelete || el.colors !== colorsDelete
+                );
+    
+                localStorage.setItem("product", JSON.stringify(saveProduct));
+    
+                //Alerte produit supprimé et refresh
+                alert("Ce produit est supprimé de votre panier");
+    
+                location.reload();
+            });
+        }
+    }
+    productDElete();
 
 
- 
+
 
 
